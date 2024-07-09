d910ef6afe80077345d4ce62732085065ac68efe
aoypsk8 <108444410+aoypsk8@users.noreply.github.com>
2024-07-09 09:42:21 +0700

    Update least2

--- routes/router.js
+++ routes/router.js
@@ -1,49 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const dotenv = require('dotenv');
 const { Client } = require('pg');
-
-// Database connection details
+// Config env
+dotenv.config();
 const dbConfig = {
-    host: '172.28.26.8',
-    database: 'lmm',
-    user: 'lmm',
-    password: 'Mm0ney@2024',
-    port: 5432,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 };
 
+// usersFix
+const usersFix = [
+    { username: 'admin', password: '123456' },
+    { username: 'user1', password: 'user1' },
+    { username: 'user2', password: 'user2' },
+    { username: 'user3', password: 'user3' },
+    { username: 'user4', password: 'user4' }
+];
+//First page
 router.get("/", (req, res) => {
     res.render('Login');
 });
 
+// Login Logic
 router.post('/doLogin', (req, res) => {
     const { username, password } = req.body;
-    console.log('Received login request:', { username, password });
-
-    // Replace with your own authentication logic
-    if (username === '1' && password === '1') {
-        res.json({ success: true, redirectUrl: '/dashboard' });
+    const user = usersFix.find(u => u.username === username && u.password === password);
+    if (user) {
+        res.json({ success: true, message: 'ເຂົ້າສູ່ລະບົບສຳເລັດ', redirectUrl: '/dashboard' });
     } else {
         res.json({ success: false, message: 'ຊື່ຜູ້ໃຊ້ ຫຼື ລະຫັດຜ່ານບໍ່ຖືກ !' });
     }
 });
 
 router.get('/dashboard', async (req, res) => {
-    const client = new Client(dbConfig); // Create a new client instance
-
+    const client = new Client(dbConfig); 
     try {
-        await client.connect(); // Connect to PostgreSQL
-        // Define your query
-        const query = `
-            SELECT id, codes, amount, active, created, remark, tranid, msisdn, tran_date, startdate, enddate, title, location
-            FROM public."Redeem" WHERE active = false;
-        `;
-
+        await client.connect(); 
+        const query = ` SELECT * FROM public."Redeem" WHERE active = false;`;
         // Execute the query
         const result = await client.query(query);
         const data = result.rows; 
-        // Render the 'Dashboard' view with the retrieved data
+        console.log(data);
+        
         res.render('Dashboard', { data });
-
     } catch (err) {
         console.error('Error executing query', err);
         res.status(500).send('Error fetching data');
