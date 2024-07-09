f4602b88bdaeb022ec84244a47f17a4164f8bbbe
aoypsk8 <108444410+aoypsk8@users.noreply.github.com>
2024-07-09 10:50:26 +0700

    Update least3

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const dotenv = require('dotenv');
+const session = require('express-session');
 const { Client } = require('pg');
 // Config env
 dotenv.config();
@@ -12,13 +13,27 @@ const dbConfig = {
     port: process.env.DB_PORT,
 };
 
+router.use(session({
+    secret: 'MMoneyX',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Change to true if using HTTPS
+}));
+
 // usersFix
 const usersFix = [
-    { username: 'admin', password: '123456' },
-    { username: 'user1', password: 'user1' },
-    { username: 'user2', password: 'user2' },
-    { username: 'user3', password: 'user3' },
-    { username: 'user4', password: 'user4' }
+    { username: 'adminMMoney', password: 'mmoney123456' },
+    { username: 'Sapphire78', password: 'Cascade@27' },
+    { username: 'LunaStar43', password: 'Moonlight#21' },
+    { username: 'Phoenix87', password: 'Firebird$99' },
+    { username: 'AuroraSky22', password: 'Celestial*56' },
+    { username: 'Emerald99', password: 'Jade@77' },
+    { username: 'Stellar75', password: 'Galaxy%33' },
+    { username: 'EchoWave18', password: 'Whisper+92' },
+    { username: 'AmberSunset', password: 'Golden&25' },
+    { username: 'SilverDragon', password: 'Knight@64' },
+    { username: 'RubyStar71', password: 'Diamond#88' },
+    { username: 'Neptune55', password: 'Trident$39' }
 ];
 //First page
 router.get("/", (req, res) => {
@@ -30,6 +45,7 @@ router.post('/doLogin', (req, res) => {
     const { username, password } = req.body;
     const user = usersFix.find(u => u.username === username && u.password === password);
     if (user) {
+        req.session.isAuthenticated = true;
         res.json({ success: true, message: 'ເຂົ້າສູ່ລະບົບສຳເລັດ', redirectUrl: '/dashboard' });
     } else {
         res.json({ success: false, message: 'ຊື່ຜູ້ໃຊ້ ຫຼື ລະຫັດຜ່ານບໍ່ຖືກ !' });
@@ -37,6 +53,11 @@ router.post('/doLogin', (req, res) => {
 });
 
 router.get('/dashboard', async (req, res) => {
+    if (!req.session.isAuthenticated) {
+        res.render('NotFound');
+        return;
+    }
+
     const client = new Client(dbConfig); 
     try {
         await client.connect(); 
@@ -57,6 +78,10 @@ router.get('/dashboard', async (req, res) => {
             console.error('Error ending client', err);
         }
     }
+});
+// Not Found handler for other routes
+router.get('*', (req, res) => {
+    res.render('NotFound');
 });
 
 module.exports = router;
